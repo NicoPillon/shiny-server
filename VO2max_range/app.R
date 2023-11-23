484e95ab89972369c0c9f4e58a86aa516e0a839d
Nicolas Pillon <nicolas.pillon@ki.se>
2023-11-23 09:46:47 +0100

    Added VO2max range dataset

---
+++ VO2max_range/app.R
@@ -0,0 +1,187 @@
+#-----------------------------------------------------------------------
+#
+# Reference values for VO2 max
+#
+#----------------------------------------------------------------------
+# Load data and libraries
+library(shinycssloaders)
+library(ggplot2)
+library(ggrepel)
+library(DT)
+library(cowplot)
+library(rlang)
+
+#--------------------------------------------------------------------------------------------------------
+# Palmitate
+#--------------------------------------------------------------------------------------------------------
+VO2_data <- readRDS("data/data.Rds")
+
+# make factor to place mean in between percentiles
+VO2_data$Percentile <- factor(VO2_data$Percentile,
+                              levels = c("Percentile 0.9", "Percentile 0.75", "Percentile 0.6",
+                                         "Mean", 
+                                         "Percentile 0.4", "Percentile 0.25", "Percentile 0.1"))
+# lsit of studies
+studies <- readRDS("data/studies.Rds")
+
+# function for plots
+fct_nomogram <- function(dat){
+  ggplot(dat, aes(x = Age, y = VO2max,
+                       color = Percentile, linetype = Percentile)) +
+    theme_bw(16) +
+    theme(panel.grid.major = element_line(color = "gray80",
+                                          linewidth = 0.5,
+                                          linetype = 1),
+          legend.title = element_blank()) +
+    scale_x_continuous(breaks = seq(0, 100, 10),
+                       minor_breaks = seq(0, 100, 1)) +
+    scale_y_continuous(breaks = seq(0, 100, 10),
+                       minor_breaks = seq(0, 100, 1)) +
+    labs(x = "Age (Years)",
+         y = "VO2max (mL/min/kg)") +
+    #facet_wrap(.~Sex, scales = "free_y", ncol = 2) +
+    geom_smooth(se = FALSE, method = "lm", linewidth = 0.5, alpha = 0.9) +
+    scale_colour_manual(values = c("#2E7F18", "#45731E", "#675E24",
+                                   "black",
+                                   "#8D472B", "#B13433", "#C82538")) +
+    scale_linetype_manual(values = c(4,3,2,1,2,3,4))
+}
+
+# Define UI ----
+ui <- fluidPage(theme = "bootstrap.css",
+                fluidRow(style="color:white;background-color:#5b768e;padding:0% 1% 1% 1%;text-align:center",
+                         h3("Reference values for maximal oxygen uptake"),
+                         h5("By", a("Nicolas J. Pillon", href="https://staff.ki.se/people/nicolas-pillon", 
+                                    target="_blank", style="color:#D9DADB"), 
+                            "/ last update 2023-11-22")
+                ),
+
+                sidebarLayout(
+                  sidebarPanel(width = 2,
+                               selectInput("sex",
+                                           label = "Sex", 
+                                           selected = "Female", 
+                                           choices = c("Female", "Male")),
+                               numericInput("age",
+                                            label = "Age (years)",
+                                            value = 30),
+                               numericInput("vo2max",
+                                            label = "VO2max (mL/min/kg)",
+                                            value = 40),
+                               "The data in the plot besides was calculated from the publications listed in the table below."
+                               ),
+                  mainPanel(width = 10,
+                            plotOutput("VO2plot")
+                            )
+                ),
+                
+                fluidRow(style="padding:1% 2% 1% 2%",
+                  tags$hr(),
+                  DT::dataTableOutput("studies_table"),
+                  tags$hr(),
+                  plotOutput("studies_plot", height = 500)
+                )
+)
+
+
+# Define server logic ----
+server <- function(input, output, session) {
+  
+
+  output$VO2plot <- renderPlot({
+    
+    # input values
+    input_age = 30
+    input_vo2max = 40
+    input_sex = "Male"
+    input_age = input$age
+    input_vo2max = input$vo2max
+    input_sex = input$sex
+
+    # error messages
+    validate(need(input_age, "Please provide a numeric value age"))
+    validate(need(input_vo2max, "Please provide a numeric value for VO2max"))
+    
+    # subset selected sex
+    VO2_data_M <- subset(VO2_data, Sex == "Male")
+    VO2_data_F <- subset(VO2_data, Sex == "Female")
+    
+    # plot
+    nomogram_legend <- get_legend(
+      fct_nomogram(VO2_data_M) + 
+        guides(color = guide_legend(ncol = 1)) 
+    )
+
+    # male plot
+    nomogram_M <- fct_nomogram(VO2_data_M) + 
+      labs(subtitle = "Male") +
+      theme(legend.position = "none")
+    
+    nomogram_M <- if(input_sex == "Male")
+      nomogram_M + 
+      annotate("label", 
+               x = input_age, 
+               y = input_vo2max -2, 
+               label = "Your VO2max", 
+               color = "red") +
+      annotate("pointrange", 
+               x = input_age, 
+               y = input_vo2max, 
+               ymin = input_vo2max, ymax = input_vo2max,
+               color = "red", 
+               size = 0.5) else nomogram_M
+    
+    # female plot
+    nomogram_F <- fct_nomogram(VO2_data_F) + 
+      labs(subtitle = "Female") +
+      theme(legend.position = "none")
+    
+    nomogram_F <- if(input_sex == "Female")
+      nomogram_F + 
+      annotate("label", 
+               x = input_age, 
+               y = input_vo2max -2, 
+               label = "Your VO2max", 
+               color = "red") +
+      annotate("pointrange", 
+               x = input_age, 
+               y = input_vo2max, 
+               ymin = input_vo2max, ymax = input_vo2max,
+               color = "red", 
+               size = 0.5) else nomogram_F
+    
+    
+    # merge plots
+    cowplot::plot_grid(
+      nomogram_F,
+      nomogram_M,
+      nomogram_legend,
+      ncol = 3,
+      rel_widths = c(2,2,1)
+    )
+  })
+  
+  output$studies_table <- DT::renderDataTable(escape = FALSE, 
+                                              rownames = FALSE, 
+                                              options=list(paging = FALSE), 
+                                              { studies })
+  
+  output$studies_plot <- renderPlot({
+    # plot mean only for all studies
+    ggplot(VO2_data[VO2_data$Percentile == "Mean",], 
+           aes(x = Age, y = VO2max, 
+               color = Reference, shape = Reference)) +
+      theme_bw(16) +
+      theme(legend.key.size = unit(20, "pt")) +
+      labs(subtitle = "Mean VO2max by age") +
+      facet_wrap(.~Sex) +
+      geom_point(size = 1.5) +
+      geom_line(linewidth = 0.25) +
+      scale_x_continuous(breaks = seq(20, 90, 10)) +
+      scale_shape_manual(values = rep(c(15,16,17,18), 20))
+  })
+
+}
+
+# Run the app ----
+shinyApp(ui = ui, server = server)
---
+++ VO2max_range/preprocessing.R
@@ -0,0 +1,23 @@
+library(rstudioapi)
+setwd(dirname(getActiveDocumentContext()$path))
+##########################################################################################################
+#
+# Collect source data for shiny app 
+#
+##########################################################################################################
+library(readxl)
+
+# data
+saveRDS(readRDS("../../R_databases/VO2_range/data/pool_data.Rds"),
+        file="data/data.Rds")
+
+
+studies <- read_xlsx("../../R_databases/VO2_range/publications/000_Studies.xlsx")[,c(1:4,6)]
+
+
+createLink <- function(val) {
+  sprintf(paste0('<a href="', URLdecode(val),'" target="_blank">', gsub("(.*org/)|(.*=)", "", val) ,'</a>'))
+}
+studies$DOI  <- sapply(studies$DOI, createLink)
+
+saveRDS(studies, file = "data/studies.Rds")
